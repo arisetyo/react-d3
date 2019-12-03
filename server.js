69bd2638064ac85865c9dc4082089792304dd383
Arie M. Prasetyo <ari.setyo@gmail.com>
2019-12-03 21:45:51 +0800

    first batch

---
+++ server.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright: 	UI/X : AMP (2019)
+ * Author: 			Arie M. Prasetyo
+ * Created:			2019-08-26
+ */
+ 
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+
+/**
+ * Webpack dev server implementation to allow for React Hot Loader usage.
+ *
+ * Sourced from https://github.com/gaearon/react-hot-boilerplate
+ */
+new WebpackDevServer(webpack(config), {
+	publicPath: config.output.publicPath,
+	hot: true,
+	historyApiFallback: true,
+	headers: { 'Access-Control-Allow-Origin': '*' }
+}).listen(config.devServer.port, config.devServer.host, function(err, result) {
+	if (err) {
+		//noinspection Eslint
+		console.log(err); // eslint-disable-line
+		process.exit(1);
+	}
+	console.log('Listening at http://' + config.devServer.host + ':' + config.devServer.port); // eslint-disable-line
+});
---
+++ src/charts/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Arie on 2019-03-22
+ */
+
+import AreaChart from './AreaChart';
+import LineChart from './LineChart';
+import SparkLine from './SparkLine';
+
+export {
+	AreaChart,
+	LineChart,
+	SparkLine
+};
